9a0f67a088158e96a91946cd03cc57d3d0f843ef
user <duty_bound@aol.com>
2019-01-31 22:26:32 +0100

    Backup 20190131

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ import ReactDOM from 'react-dom';
 const mountNode = document.getElementById('mountNode');
 const component = <h1>Hello World!</h1>;
 
+const c1;
+
 ReactDOM.render(component, mountNode);
