03c9ea4f2ce2f5322a8cde53ccc658086bbbd11a
user <duty_bound@aol.com>
2019-05-16 21:45:41 +0200

    fix: console logs mmm2

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 const mountNode = document.getElementById('mountNode');
 const component = <h1>Hello World!</h1>;
 
-console.log("Here we go!!!")
+console.log("Here we gowith mmm2!!!")
 
 ReactDOM.render(component, mountNode);
