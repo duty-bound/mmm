58ba0c939bf82a04c9062cbf3fcf986e7798727b
user <duty_bound@aol.com>
2019-05-16 21:22:01 +0200

    feat: change1

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ import ReactDOM from 'react-dom';
 const mountNode = document.getElementById('mountNode');
 const component = <h1>Hello World!</h1>;
 
+console.log("Here we go!!!")
+
 ReactDOM.render(component, mountNode);
